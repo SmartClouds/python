7d792159f6c8ad57fbe746ab1bf2f48d86448b97
ajafarip <96071907+ajafarip@users.noreply.github.com>
2024-09-22 19:52:02 -0400

    Add dictionaries

--- planet.py
+++ planet.py
@@ -71,3 +71,53 @@ for number in countdown:
     print(number)
     sleep(2)  # Wait 1 second
 print("Blast off!! 🚀")
+
+################# Dictionaries 
+
+planet = {
+    'name':'Mars',
+    'moons': 2
+    }
+print(f'{planet["name"]} has {planet["moons"]} moon(s)')
+
+planet['circumference (km)'] = {
+    'polar': 6752,
+    'equatorial': 6792
+}
+
+print(f'{planet["name"]} has a polar circumference of {planet["circumference (km)"]["polar"]}')
+
+
+rainfall = {
+    'october': 3.5,
+    'november': 4.2,
+    'december': 2.1
+}
+for key in rainfall.keys():
+    print(f'{key}: {rainfall[key]}cm')
+
+
+planet_moons = {
+    'mercury': 0,
+    'venus': 0,
+    'earth': 1,
+    'mars': 2,
+    'jupiter': 79,
+    'saturn': 82,
+    'uranus': 27,
+    'neptune': 14,
+    'pluto': 5,
+    'haumea': 2,
+    'makemake': 1,
+    'eris': 1
+}
+moons=planet_moons.values()
+total_planets= len(planet_moons.keys())    
+
+total_moons = 0
+for moon in moons:
+    total_moons = total_moons + moon
+
+average = total_moons / total_planets
+
+print(f'Each planet has an average of {average} moons')
